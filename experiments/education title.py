530ecc2554f240f8580835f9904b01eb6c8cf461
asish012 <asish012@gmail.com>
2022-05-02 15:00:21 +0200

    analysis 2

---
+++ experiments/education title.py
@@ -0,0 +1,25 @@
+# education title
+education_map = {
+    "Doktor"                                : "Ph.D.",
+    "Doctor of Philosophy"                  : "Ph.D.",
+    "Master of Science"                     : "Masters",
+    "Master of Engineering"                 : "Masters",
+    "Master's Degree"                       : "Masters",
+    "Master of Arts"                        : "Masters",
+    "Master"                                : "Masters",
+    "Msc"                                   : "Masters",
+    "M. Sc."                                : "Masters",
+    "Bachelor of Science"                   : "Bachelors",
+    "Bachelor's Degree"                     : "Bachelors",
+    "Bachelor of Engineering"               : "Bachelors",
+    "Bachelor of Arts"                      : "Bachelors",
+    "Bachelor of Technology"                : "Bachelors",
+    "Bachelor of Applied Science"           : "Bachelors",
+    "Bachelor"                              : "Bachelors",
+    "B.Sc."                                 : "Bachelors",
+    "Bs"                                    : "Bachelors",
+    "Master of Business Administration"     : "MBA",
+    "Bachelor of Business Administration"   : "BBA"
+}
+
+&amp;                       &
